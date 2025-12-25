1983aa28438dd1374e616409489f8fdaead4663f
MemreY <memreyildirim0660@gmail.com>
2025-12-25 16:24:03 +0300

    Update Euler-3.py

--- Euler/Euler-3.py
+++ Euler/Euler-3.py
@@ -1,7 +1,8 @@
-number = 13195
+number = 13195 #change the number which is asked in the problem
 prime_factors = [
     i for i in range(2, number+1)
     if number % i == 0 and all(i % j != 0 for j in range(2, int(i**0.5)+1))
 ]
+
 
 print("max prime is =", max(prime_factors))
